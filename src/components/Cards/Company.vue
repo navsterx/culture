206fc86556afdfc75f748c4da13e18d6313a8008
navsterx <danjwilkinson@gmail.com>
2023-10-12 17:25:44 +0100

    domain change

--- src/components/Cards/Company.vue
+++ src/components/Cards/Company.vue
@@ -10,7 +10,7 @@
             role{{ company.jobs.length === 1 ? '' : 's' }} available
           </div>
           <div class="text-body-2 text-textPrimary" v-else>
-            No jobs available right now
+            No roles available right now
           </div>
         </div>
       </div>
--- src/router/index.js
+++ src/router/index.js
@@ -5,7 +5,7 @@ const routes = [
   {
     path: '/',
     component: () => {
-      if (window.location.hostname === 'www.awesomeculture.co.uk') {
+      if (window.location.hostname === 'www.awesomeculture.co.uk' || window.location.hostname === 'www.awesomeculture.io') {
         // If the hostname is 'www.awesomeculture.co.uk', use the 'coming soon' layout
         return import('@/layouts/default/ComingSoon.vue');
       } else {
