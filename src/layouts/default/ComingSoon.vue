b96494b2622481367e088c4f3d6582758361e0ff
navsterx <danjwilkinson@gmail.com>
2023-10-12 18:45:30 +0100

    Update ComingSoon.vue

--- src/layouts/default/ComingSoon.vue
+++ src/layouts/default/ComingSoon.vue
@@ -1,6 +1,6 @@
 <template>
   <v-app class="body">
-    <v-row align="center" justify="center" class="full-height">
+    <v-row align="center" justify="center" class="ma-0">
       <v-col cols="12">
         <div class="d-flex justify-center">
           <v-img src="/logo.png" :max-width="300" />
@@ -17,12 +17,4 @@
 
 </script>
 
-<style>
-.body {
-  height: 100vh;
-}
-
-.full-height {
-  height: 100%;
-}
-</style>
+<style></style>
