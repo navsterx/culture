1e23048ef535e38b52b7e3c0ca1a7ccfaf249e01
navsterx <danjwilkinson@gmail.com>
2023-10-17 21:19:15 +0100

    textSecondary

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -32,6 +32,7 @@ export default createVuetify({
           primary: '#67b583',
           secondary: '#b56799',
           textPrimary: "#232323",
+          textSecondary: '#878787',
           lightGrey: "#f5f5f5",
         },
       },
