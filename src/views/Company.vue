18dcb0afbd984f6cdd5ff3bf92d121ef4c99ff0b
navsterx <danjwilkinson@gmail.com>
2024-02-20 12:16:41 +0000

    tweaks to header image

--- src/views/Company.vue
+++ src/views/Company.vue
@@ -1,71 +1,83 @@
 <template>
   <div class="p-company">
     <v-fade-transition>
-      <div v-if="isLoaded">
-        <v-sheet class="p-company__header" :color="company.brand_color">
-          <iframe :src="mapSrc" width="100%" height="325" style="border:0" loading="lazy" allowfullscreen
-            referrerpolicy="no-referrer-when-downgrade"></iframe>
-        </v-sheet>
-        <v-sheet class="pa-4" color="white" min-height="125"
-          style="position: relative; border-bottom: 1px solid lightgrey">
-          <v-container>
-            <v-row class="p-company__social">
-              <v-col cols="12" lg="4" md="4" sm="12">
-                <div class="d-flex justify-center">
-                  <v-img :src="company.logo" :max-width="175" :max-height="175" eager style="z-index: 1"
-                    class="p-company__logo rounded-circle" />
-                </div>
-              </v-col>
-              <v-col cols="12" lg="8" md="8" sm="12">
-                <div>
-                  <div class="text-h5 font-weight-medium mb-2 text-textPrimary">
-                    {{ company.name }}
-                  </div>
-                  <div class="text-body-2 mb-4">
-                    {{ company.description }}
-                  </div>
-                  <div class="d-flex text-body-2 p-company__social-container">
-                    <div v-if="company.website">
-                      <div class="d-flex align-center">
-                        <div class="mr-1">
-                          <v-icon :min-width="14" :min-height="14">mdi-home</v-icon>
-                        </div>
-                        <a :href="company.website" class="text-decoration-none text-textSecondary" target="_blank">{{
-                          company.name }}</a>
-                      </div>
+      <div v-if="isLoaded" class="p-company__hero d-flex align-center"
+        :style="{ 'background-image': 'url(' + company.header_image + ')' }">
+        <v-container class="d-flex justify-center">
+          <div style="width: 80%">
+            <div class="p-company__hero-text">
+              <div class="text-h4 font-weight-bold mb-2">
+                {{ company.name }}
+              </div>
+              <div class="text-body-1 mb-4">
+                {{ company.description }}
+              </div>
+              <div class="d-flex text-body-1 p-company__social-container">
+                <div v-if="company.website">
+                  <div class="d-flex align-center">
+                    <div class="mr-1">
+                      <v-icon :min-width="18" :min-height="18">mdi-home</v-icon>
                     </div>
-                    <div v-if="company.social_linkedin">
-                      <div class="d-flex align-center">
-                        <div class="mr-2">
-                          <v-img eager class="ml-1" src="/images/social/linkedin.png" :min-width="14" :min-height="14" />
-                        </div>
-                        <a :href="`https://www.linkedin.com/company/${company.social_linkedin}`"
-                          class="text-decoration-none text-textSecondary" target="_blank">{{
-                            company.social_linkedin }}</a>
-                      </div>
-                    </div>
-                    <div v-if="company.social_x" class="d-flex align-center">
-                      <div class="mr-2">
-                        <v-img eager class="ml-1" src="/images/social/logo-black.png" :min-width="14" :min-height="14" />
-                      </div>
-                      <div><a :href="`https://www.x.com/${company.social_x}`" target="_blank"
-                          class="text-decoration-none text-textSecondary">{{
-                            company.social_x }}</a>
-                      </div>
-                    </div>
+                    <a :href="company.website" class="text-decoration-none p-company__hero-text" target="_blank">{{
+                      company.name }}</a>
                   </div>
                 </div>
-              </v-col>
-            </v-row>
-          </v-container>
-        </v-sheet>
+                <div v-if="company.social_linkedin">
+                  <div class="d-flex align-center">
+                    <div class="mr-2">
+                      <v-img eager class="ml-1" src="/images/social/linkedin-white.png" :min-width="18"
+                        :min-height="18" />
+                    </div>
+                    <a :href="`https://www.linkedin.com/company/${company.social_linkedin}`"
+                      class="text-decoration-none p-company__hero-text" target="_blank">{{
+                        company.social_linkedin }}</a>
+                  </div>
+                </div>
+                <div v-if="company.social_x" class="d-flex align-center">
+                  <div class="mr-2">
+                    <v-img eager class="ml-1" src="/images/social/x-white.png" :min-width="18" :min-height="18" />
+                  </div>
+                  <div><a :href="`https://www.x.com/${company.social_x}`" target="_blank"
+                      class="text-decoration-none p-company__hero-text">{{
+                        company.social_x }}</a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </v-container>
       </div>
     </v-fade-transition>
     <v-container>
       <v-responsive>
         <v-fade-transition>
           <v-row class="pa-1" v-if="isLoaded">
+            <v-col lg="8" md="8" sm="12">
+              <v-sheet class="pa-4 mb-6 p-company__content" elevation="1" rounded v-for="(item, index) in company.content"
+                :key="index" :class="{ 'mb-6': index !== company.content.length - 1 }">
+                <sidebar-item :title="`${item.title} at ${company.name}?`" />
+                <v-sheet class="text-body-2 font-weight-regular mt-4" v-html="item.content"></v-sheet>
+              </v-sheet>
+              <v-sheet v-if="company.techstack" class="pa-4 p-company__content" elevation="1" rounded>
+                <sidebar-item :title="`What's the tech stack at ${company.name}?`" />
+                <div class="p-company__stack d-flex align-center  flex-wrap">
+                  <div v-for="(tech, index) in company.techstack" :key="index">
+                    <v-tooltip :text="tech.name" location="bottom">
+                      <template v-slot:activator="{ props }">
+                        <i v-bind="props" class="colored" :class="tech.class"></i>
+                      </template>
+                    </v-tooltip>
+                  </div>
+                </div>
+              </v-sheet>
+            </v-col>
             <v-col cols="12" lg="4" md="4" sm="12">
+              <v-fade-transition>
+                <div v-if="isLoaded">
+                  <iframe class="mb-4" :src="mapSrc" width="100%" height="275" style="border:0" allowfullscreen
+                    referrerpolicy="no-referrer-when-downgrade"></iframe>
+                </div>
+              </v-fade-transition>
               <v-sheet class="pa-4 mb-6" elevation="1" v-if="company.perks" rounded>
                 <sidebar-item title="Benefits & Perks" />
                 <perk v-for="(perk, index) in company.perks" :key="index" :perk="perk"
@@ -84,25 +96,6 @@
                   variant="text" color="secondary">
                   Show More
                 </v-btn>
-              </v-sheet>
-            </v-col>
-            <v-col lg="8" md="8" sm="12">
-              <v-sheet class="pa-4 mb-6 p-company__content" elevation="1" rounded v-for="(item, index) in company.content"
-                :key="index" :class="{ 'mb-6': index !== company.content.length - 1 }">
-                <sidebar-item :title="`${item.title} at ${company.name}?`" />
-                <v-sheet class="text-body-2 font-weight-regular mt-4" v-html="item.content"></v-sheet>
-              </v-sheet>
-              <v-sheet v-if="company.techstack" class="pa-4 p-company__content" elevation="1" rounded>
-                <sidebar-item :title="`What's the tech stack at ${company.name}?`" />
-                <div class="p-company__stack d-flex align-center  flex-wrap">
-                  <div v-for="(tech, index) in company.techstack" :key="index">
-                    <v-tooltip :text="tech.name" location="bottom">
-                      <template v-slot:activator="{ props }">
-                        <i v-bind="props" class="colored" :class="tech.class"></i>
-                      </template>
-                    </v-tooltip>
-                  </div>
-                </div>
               </v-sheet>
             </v-col>
           </v-row>
@@ -190,19 +183,6 @@ const mapSrc = computed(() => {
     gap: 24px;
   }
 
-  &__header {
-    height: 325px;
-
-    @media (max-width: 959px) {
-      height: 325px;
-    }
-  }
-
-  &__logo {
-    border: 3px solid #fff;
-    box-shadow: 0 8px 12px 0 rgba(0, 0, 0, 0.096), 0 6px 20px 0 rgba(0, 0, 0, 0.082) !important;
-  }
-
   &__social-container {
     @media (max-width: 959px) {
       flex-direction: column;
@@ -214,6 +194,30 @@ const mapSrc = computed(() => {
 
   &__social-icons {
     text-decoration: none;
+  }
+
+  &__hero {
+    min-height: 425px;
+    background-size: cover;
+    background-position: top;
+    position: relative;
+  }
+
+  &__hero::before {
+    content: "";
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-image: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5));
+    pointer-events: none;
+  }
+
+  &__hero-text {
+    position: relative;
+    z-index: 1;
+    color: #fff;
   }
 
   &__content {
