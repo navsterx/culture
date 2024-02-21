6496b312ddd0623274f425625642c46973127d04
navsterx <danjwilkinson@gmail.com>
2024-02-21 14:00:10 +0000

    Update SidebarItem.vue

--- src/components/Cards/SidebarItem.vue
+++ src/components/Cards/SidebarItem.vue
@@ -3,7 +3,7 @@
     <div class="text-subtitle-1 font-weight-medium">
       {{ title }}
     </div>
-    <v-divider class="mt-1 mb-2" />
+    <v-divider class="mt-1 mb-3" />
   </div>
 </template>
 
